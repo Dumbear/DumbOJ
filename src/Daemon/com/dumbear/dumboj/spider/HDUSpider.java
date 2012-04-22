4d3078b3b94725584333c2a6278e9677a176ec1c
Liang Yaxiong <dumbear@163.com>
2012-04-22 16:25:04 +0800

    Fixed HDUSpider.

--- src/Daemon/com/dumbear/dumboj/spider/HDUSpider.java
+++ src/Daemon/com/dumbear/dumboj/spider/HDUSpider.java
@@ -23,19 +23,19 @@ public class HDUSpider extends Spider {
         if (problem.getTitle().isEmpty()) {
             throw new Exception("Problem not available");
         }
-        problem.setSource(Utility.getMatcherString(source, "Source</div> <div class=panel_content>([\\s\\S]*?)<[^<>]*?panel_[^<>]*?>", 1));
+        problem.setSource(Utility.getMatcherString(source, "<div class=panel_title align=left>Source</div> <div class=panel_content>([\\s\\S]*?)</div> <div class=panel_bottom>&nbsp;</div>", 1));
         problem.setOriginalUrl("http://acm.hdu.edu.cn/showproblem.php?pid=" + id);
         problem.setOriginalSite(SITE);
         problem.setOriginalId(id);
         problem.setTimeLimit(Integer.parseInt(Utility.getMatcherString(source, "(\\d*) MS", 1)));
         problem.setMemoryLimit(Integer.parseInt(Utility.getMatcherString(source, "/(\\d*) K", 1)));
         problemContent.setUserId(1);
-        problemContent.setDescription(Utility.getMatcherString(source, "Problem Description</div>([\\s\\S]*?)<br><[^<>]*?panel_title[^<>]*?>", 1));
-        problemContent.setInput(Utility.getMatcherString(source, "Input</div>([\\s\\S]*?)<br><[^<>]*?panel_title[^<>]*?>", 1));
-        problemContent.setOutput(Utility.getMatcherString(source, "Output</div>([\\s\\S]*?)<br><[^<>]*?panel_title[^<>]*?>", 1));
-        problemContent.setSampleInput(Utility.getMatcherString(source, "Sample Input</div>([\\s\\S]*?)<br><[^<>]*?panel_title[^<>]*?>", 1));
-        problemContent.setSampleOutput(Utility.getMatcherString(source, "Sample Output</div>([\\s\\S]*?)(<br><[^<>]*?panel_title[^<>]*?>|<[^<>]*?><[^<>]*?><i>Hint)", 1));
-        problemContent.setHint(Utility.getMatcherString(source, "<i>Hint</i></div>([\\s\\S]*?)<br><[^<>]*?panel_title[^<>]*?>", 1));
+        problemContent.setDescription(Utility.getMatcherString(source, "<div class=panel_title align=left>Problem Description</div> <div class=panel_content>([\\s\\S]*?)</div><div class=panel_bottom>&nbsp;</div>", 1));
+        problemContent.setInput(Utility.getMatcherString(source, "<div class=panel_title align=left>Input</div> <div class=panel_content>([\\s\\S]*?)</div><div class=panel_bottom>&nbsp;</div>", 1));
+        problemContent.setOutput(Utility.getMatcherString(source, "<div class=panel_title align=left>Output</div> <div class=panel_content>([\\s\\S]*?)</div><div class=panel_bottom>&nbsp;</div>", 1));
+        problemContent.setSampleInput(Utility.getMatcherString(source, "<div class=panel_title align=left>Sample Input</div><div class=panel_content>([\\s\\S]*?)</div><div class=panel_bottom>&nbsp;</div>", 1));
+        problemContent.setSampleOutput(Utility.getMatcherString(source, "<div class=panel_title align=left>Sample Output</div><div class=panel_content>([\\s\\S]*?)(<[^<>]*?><[^<>]*?><i>Hint|</div><div class=panel_bottom>&nbsp;</div>)", 1));
+        problemContent.setHint(Utility.getMatcherString(source, "<i>Hint</i></div>([\\s\\S]*?)</div><i style='font-size:1px'> </i>", 1));
         problemContent.setRemark("Created by " + SITE + "Spider.");
     }
 }
